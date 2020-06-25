e6565162c5954909374a6392a285dce4037990d9
Miguel Lorenzo <mlorenzo@alicebiometrics.com>
2020-06-25 16:12:08 +0200

    Added invoke dependecy to setup

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
     provides=["imageio_ffmpeg"],
     python_requires=">=3.4",
     setup_requires=["pip>19"],
-    install_requires=[],
+    install_requires=["invoke"],
     packages=["imageio_ffmpeg"],
     package_dir={"imageio_ffmpeg": "imageio_ffmpeg"},
     package_data={"imageio_ffmpeg": ["binaries/*.*"]},
